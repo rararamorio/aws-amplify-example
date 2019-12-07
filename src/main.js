3cbbb5e951c025116b13cc4a7b3c90b7181b808e
rararamorio <rararamorio@gmail.com>
2019-12-07 10:08:28 +0900

    認証処理を追加した

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
 
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   render: h => h(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,56 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import { components, AmplifyEventBus, AmplifyPlugin } from "aws-amplify-vue";
+import * as AmplifyModules from "aws-amplify";
+
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+let user;
+
+getUser().then((user, error) => {
+  console.log(error);
+  if (user) {
+    router.push({ path: "/" });
+  }
+});
+
+AmplifyEventBus.$on("authState", async state => {
+  if (state === "signedOut") {
+    user = null;
+    router.push({ path: "/auth" });
+  } else if (state === "signedIn") {
+    user = await getUser();
+    router.push({ path: "/" });
+  }
+});
+
+function getUser() {
+  return Vue.prototype.$Amplify.Auth.currentAuthenticatedUser()
+    .then(data => {
+      if (data && data.signInUserSession) {
+        return data;
+      }
+    })
+    .catch(e => {
+      console.log(e);
+      return null;
+    });
+}
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/auth",
+    name: "Authenticator",
+    component: components.Authenticator
   },
   {
     path: "/about",
@@ -25,6 +67,21 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeResolve(async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    user = await getUser();
+    if (!user) {
+      return next({
+        path: "/auth",
+        query: {
+          redirect: to.fullPath
+        }
+      });
+    }
+  }
+  return next();
 });
 
 export default router;
