98aab3871a335994e4d6c4e4ca03c6aa40e91911
rararamorio <rararamorio@gmail.com>
2020-02-04 22:06:50 +0900

    認証画面の日本語化対応を行った

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Amplify from "aws-amplify";
+import { I18n } from "aws-amplify";
+import { messages } from "./i18n/amplify/messages";
 import aws_exports from "./aws-exports";
 
 Amplify.configure(aws_exports);
+I18n.putVocabularies(messages);
+I18n.setLanguage("ja");
 
 Vue.config.productionTip = false;
 new Vue({
